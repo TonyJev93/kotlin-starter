4e9ab3105cedf4b80993d0a335ed733279b4baba
전계원 <gary@jeongyewon-ui-MacBookAir.local>
2023-10-22 20:16:38 +0900

    feat: add dto validation check

--- src/main/kotlin/com/tonyjev93/kotlin/starter/controller/TestRestController.kt
+++ src/main/kotlin/com/tonyjev93/kotlin/starter/controller/TestRestController.kt
@@ -2,12 +2,8 @@ package com.tonyjev93.kotlin.starter.controller
 
 import com.tonyjev93.kotlin.starter.controller.dto.TestDto
 import com.tonyjev93.kotlin.starter.service.TestService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(("/test"))
@@ -19,7 +15,7 @@ class TestRestController(private val testService: TestService) {
     }
 
     @PostMapping
-    fun postTest(@RequestBody request: TestDto.Req): TestDto.Resp {
+    fun postTest(@Valid @RequestBody request: TestDto.Req): TestDto.Resp {
         return testService.post(request)
     }
 }
--- src/main/kotlin/com/tonyjev93/kotlin/starter/controller/dto/TestDto.kt
+++ src/main/kotlin/com/tonyjev93/kotlin/starter/controller/dto/TestDto.kt
@@ -1,11 +1,23 @@
 package com.tonyjev93.kotlin.starter.controller.dto
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Pattern
+
 class TestDto {
 
     data class Req(
-            val firstName: String,
+            @field:NotBlank
+            val firstName: String?,
+
+            @field:NotEmpty
             val lastName: String,
+
+            @field:Email
             val email: String,
+
+            @field:Pattern(regexp = """^010-\d{4}-\d{4}$""")
             val phone: String,
     )
 
